8bcf8f9513091e74b53a7326be63fa6ad534bb64
ermiyashb <65134165+ermiyashb@users.noreply.github.com>
2022-07-02 15:24:43 +0430

    Add files via upload

---
+++ 55.js
@@ -0,0 +1,49 @@
+var a = function(){
+    console.log("Hello yaakov")
+}
+a();
+
+var b = function(){
+    console.log("Goodbye john")
+}
+b();
+
+var c = function(){
+    console.log("Goodbye jen")
+}
+c();
+
+var d = function(){
+    console.log("Goodbye jason")
+}
+d();
+
+var e = function(){
+    console.log("Hello paul")
+}
+e();
+
+var f = function(){
+    console.log("Hello frank")
+}
+f();
+
+var g = function(){
+    console.log("Hello larry")
+}
+g();
+
+var h = function(){
+    console.log("Hello paula")
+}
+h();
+
+var I = function(){
+    console.log("Hello laura")
+}
+I();
+
+var j = function(){
+    console.log("Goodbye Jim")
+}
+j();
